829a5ad9badbcf40af2ac7cc98bcabb993a66b6b
srabani Mahapatra <srabanimahapatra005@gmail.com>
2026-05-04 20:23:28 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -29,21 +29,17 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ecommerce-puce-iota.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 // app.use(
 //   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     origin: [
+//       "http://localhost:5173",
+//       "https://ecommerce-puce-iota.vercel.app"
+//     ],
 //     credentials: true,
 //   })
 // );
+
 
 // Serve static files for uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
